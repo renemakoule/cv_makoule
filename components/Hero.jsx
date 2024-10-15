21f89a21a699596e704802c1d2c6d56a90ff05b8
renemakoule <renemakoule83@gmail.com>
2024-10-16 00:17:37 +0100

    wip1

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
         <div className="flex justify-between gap-x-8 mt-10">
           {/* text */}
           <div className="flex flex-col items-center justify-center mx-auto xl:mx-0 text-center xl:text-left">
-          <p className="bg-zinc-800 hover:bg-gradient-to-r from-[#531849] via-[#07596d] to-[#dd059c] rounded-full w-full px-3 py-1 text-xs space-x-2 flex items-center justify-center lg:hidden">
+          <div className="whatsapp-status-bar hover:bg-gradient-to-r from-[#531849] via-[#07596d] to-[#dd059c] rounded-full w-full px-3 py-1 text-xs space-x-2 flex items-center justify-center lg:hidden">
                   <SparklesText 
                   text='New'
                   className="text-xs bg-white text-black hover:bg-white hover:text-[#fd4e22] rounded-full px-2"
@@ -55,7 +55,7 @@ const Hero = () => {
                    <SparklesText text="Open with a PC for a better visual"
                     className="text-xs text-white poppins-extralight-italic"
                   />
-                </p>
+                </div>
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px] mt-10">
               <BoxReveal boxColor={"#47E8FDFF"} duration={2}>
                 Web Developer, Administrator System
