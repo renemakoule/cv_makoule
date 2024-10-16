1fb38708682e5f4280cb91a6039b3141f21ff0dd
renemakoule <renemakoule83@gmail.com>
2024-10-16 22:32:00 +0100

    theme automatic

--- app/page.jsx
+++ app/page.jsx
@@ -10,6 +10,7 @@ import LoadingAnimation from '@/components/LoadingAnimation'
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
+  const [theme, setTheme] = useState('light')
 
   useEffect(() => {
     // Simuler un temps de chargement
@@ -20,12 +21,34 @@ export default function Home() {
     return () => clearTimeout(timer)
   }, [])
 
+  useEffect(() => {
+    const updateTheme = () => {
+      const currentHour = new Date().getHours()
+      const sunriseHour = 6 // 6h du matin
+      const sunsetHour = 18 // 18h du soir
+
+      if (currentHour >= sunriseHour && currentHour < sunsetHour) {
+        setTheme('light')
+      } else {
+        setTheme('dark')
+      }
+    }
+
+    // Mettre à jour le thème immédiatement
+    updateTheme()
+
+    // Mettre à jour le thème toutes les minutes
+    const interval = setInterval(updateTheme, 60000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   if (isLoading) {
     return <LoadingAnimation />
   }
 
   return (
-    <main className='main'>
+    <main className={`main ${theme}`}>
       <Hero />
       <About />
       <Service />
