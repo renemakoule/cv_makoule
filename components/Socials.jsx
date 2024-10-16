bf910fff18c931e6b398d6310fe6c2e0fcebd845
renemakoule <renemakoule83@gmail.com>
2024-10-16 07:25:08 +0100

    target

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -47,7 +47,7 @@ const Socials = ({ containerStyles, iconStyles }) => {
             exit={{ scale: 0 }}
             transition={{ delay: index * 0.1 }}
           >
-            <Link href={icon.path} target=''>
+            <Link href={icon.path} target='_blank'>
               <motion.div 
                 className={`${iconStyles}`}
                 whileHover={{ scale: 1.2, rotate: 360 }}
